2167e055498c68a9015dd9dc2c8c97de2d45d226
IN22915804 <karan.soni@tesco.com>
2021-04-17 21:50:52 +0530

    Upper bounded generics

---
+++ src/main/java/generics/bounded/ComputeAverage.java
@@ -0,0 +1,17 @@
+package generics.bounded;
+
+public class ComputeAverage<T extends Number> {
+    private T[] list;
+
+    public ComputeAverage(T[] list) {
+        this.list = list;
+    }
+
+    public double average(){
+        double  sum = 0.0;
+        for(int i=0; i<list.length; i++){
+            sum += list[i].doubleValue();
+        }
+        return sum/list.length;
+    }
+}
---
+++ src/main/java/generics/bounded/ComputeAverageTest.java
@@ -0,0 +1,13 @@
+package generics.bounded;
+
+public class ComputeAverageTest {
+    public static void main(String[] args) {
+        ComputeAverage<Integer> computeAverageInt = new ComputeAverage<>(new Integer[]{1,2,3});
+        ComputeAverage<Float> computeAverageFloat = new ComputeAverage<>(new Float[]{1.0f,2.0f,3.0f});
+        ComputeAverage<Double> computeAverageDouble = new ComputeAverage<>(new Double[]{1.0,2.0,3.0});
+
+        System.out.println(computeAverageInt.average());
+        System.out.println(computeAverageFloat.average());
+        System.out.println(computeAverageDouble.average());
+    }
+}
