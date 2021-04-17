4410b0371224424f206e7823c7b86711f03774e5
IN22915804 <karan.soni@tesco.com>
2021-04-17 22:17:26 +0530

    Umbounded wildcard

--- src/main/java/generics/wildcard/ComputeAverage.java
+++ src/main/java/generics/wildcard/ComputeAverage.java
@@ -1,4 +1,31 @@
 package generics.wildcard;
 
-public class ComputeAverage {
+/**
+ * A program to show example of wildcards
+ * @param <T>
+ */
+public class ComputeAverage<T extends Number> {
+    private T[] list;
+
+    public ComputeAverage(T[] list) {
+        this.list = list;
+    }
+
+    public double average(){
+        double  sum = 0.0;
+        for(int i=0; i<list.length; i++){
+            sum += list[i].doubleValue();
+        }
+        return sum/list.length;
+    }
+
+    /**
+     * Compare the average, and if they are equal return true
+     * else false
+     * @param computeAverage
+     * @return
+     */
+    public boolean compareAverage(ComputeAverage<?> computeAverage){
+        return this.average() == computeAverage.average();
+    }
 }
