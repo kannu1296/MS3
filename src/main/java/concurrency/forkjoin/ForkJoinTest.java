a2be7a12eec5f838428d23400709b525aaa27fba
IN22915804 <karan.soni@tesco.com>
2021-04-21 01:18:15 +0530

    Fork join commit

--- src/main/java/concurrency/forkjoin/ForkJoinTest.java
+++ src/main/java/concurrency/forkjoin/ForkJoinTest.java
@@ -6,13 +6,13 @@ public class ForkJoinTest {
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int data[] = new int[100000];
+
         for(int i=0; i<100000; i++)
             data[i] = i;
+
         Sum sum = new Sum(data, 0, 100000);
-        long time = System.currentTimeMillis();
         int result = forkJoinPool.invoke(sum);
-        time = System.currentTimeMillis() - time;
-        System.out.println(result + " " + time);
+        System.out.println(result);
         forkJoinPool.shutdown();
     }
 }
